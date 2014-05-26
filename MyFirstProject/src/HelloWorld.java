e3f39b0ed3e11b477dfc3fa040f336ee634dfe3d
Vladimir Procopciuc <svartskog@gmail.com>
2014-05-26 17:44:22 +0300

    Updated lesson06 and lesson07

--- MyFirstProject/src/HelloWorld.java
+++ MyFirstProject/src/HelloWorld.java
@@ -5,8 +5,7 @@ public class HelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello World from Inside Eclipse");
-
+		System.out.println("Hello from " + args[0]);
 	}
 
 }
